3f8d7e77de5a81e58027a4af740c42bd4a1b8e01
edwinz1 <edwinzacatelco1998@gmail.com>
2021-01-25 14:09:24 -0600

    Instalacion de Dependencias

---
+++ src/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+import 'bootstrap/dist/css/bootstrap.min.css';
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const ESLintPlugin = require('eslint-webpack-plugin')
+console.log('Ruta de Index.js: ${path.join(__dirname)}');
+module.exports = {
+    // 1.- Especificando el archivo 'index' de entrda
+    entry: path.join(__dirname,'src/index.js'),
+    //2.- Especificar el archivo de salida
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: 'budle.js'
+    },
+    // 3.- folder donde servira
+    devServer: {
+        contentBase: path.join(__dirname,'dist'),
+        port: 8082,
+        compress: true,
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test:/\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins : [new ESLintPlugin()]
+}
